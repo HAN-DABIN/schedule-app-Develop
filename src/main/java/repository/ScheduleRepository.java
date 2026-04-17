7542583619424cdebbade950a276692cafc35008
HAN-DABON <sinmint11@gmail.com>
2026-04-17 12:05:45 +0900

    feat: ScheduleRepository 생성

--- src/main/java/repository/ScheduleRepository.java
+++ src/main/java/repository/ScheduleRepository.java
@@ -1,4 +1,7 @@
 package repository;
 
-public interface ScheduleRepository {
+import entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
