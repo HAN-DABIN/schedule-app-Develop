aeb239ed5885b359fb0d9bb9703d8c50b4224280
HAN-DABON <sinmint11@gmail.com>
2026-04-17 17:38:50 +0900

    feat: UserController 생성

---
+++ src/main/java/com/example/scheduleapp2/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.example.scheduleapp2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    // 속성
+
+    // 생성자
+
+    // 기능
+}
