332cdcda52ea9dbccdca2f0fc43f80e8dc48c1bf
HAN-DABON <sinmint11@gmail.com>
2026-04-17 12:28:56 +0900

    feat: 일정 생성 API 추가

--- src/main/java/com/example/scheduleapp2/controller/ScheduleController.java
+++ src/main/java/com/example/scheduleapp2/controller/ScheduleController.java
@@ -1,7 +1,11 @@
 package com.example.scheduleapp2.controller;
 
+import com.example.scheduleapp2.dto.CreateScheduleRequest;
+import com.example.scheduleapp2.dto.CreateScheduleResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.scheduleapp2.service.ScheduleService;
@@ -20,5 +24,9 @@ public class ScheduleController {
 
     // 기능
     @PostMapping("/schedules")
-    public ResponseEntity<CreateScheduleResponse>
+    public ResponseEntity<CreateScheduleResponse> CreateSchedule(
+            @RequestBody CreateScheduleRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(request));
+    }
+            )
 }
