bc035dd3bdafaaf0b722578b22354002a2e082e0
HAN-DABON <sinmint11@gmail.com>
2026-04-17 12:03:58 +0900

    feat: scheduleEntity, baseEntity 클래스 생성

---
+++ src/main/java/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    // 속성
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+
+    // 생성자
+
+    // 기능
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+}
---
+++ src/main/java/entity/Schedule.java
@@ -0,0 +1,43 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "schedules")
+public class Schedule {
+
+    // 속성
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private String title;
+    private String contents;
+
+    // 생성자
+    protected Schedule() {}
+
+    public Schedule(Long userId, String title, String contents) {
+        this.userId = userId;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    // 기능
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+}
