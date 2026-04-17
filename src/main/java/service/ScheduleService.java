9d804fa42ee4c2bf9e3f0ebe7eea375128c8219e
HAN-DABON <sinmint11@gmail.com>
2026-04-17 12:09:00 +0900

    feat: ScheduleServie 생성

--- src/main/java/service/ScheduleService.java
+++ src/main/java/service/ScheduleService.java
@@ -1,4 +1,16 @@
 package service;
 
+import org.springframework.stereotype.Service;
+import repository.ScheduleRepository;
+
+@Service
 public class ScheduleService {
+
+    // 속성
+    private final ScheduleRepository scheduleRepository;
+
+    // 생성자
+    public ScheduleService(ScheduleRepository scheduleRepository) {
+        this.scheduleRepository = scheduleRepository;
+    }
 }
