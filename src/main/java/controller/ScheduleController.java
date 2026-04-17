9aa1c2426ac12e63054460ca4a92bf1745c7e889
HAN-DABON <sinmint11@gmail.com>
2026-04-17 12:13:23 +0900

    feat: ScheduleController 생성

--- src/main/java/controller/ScheduleController.java
+++ src/main/java/controller/ScheduleController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.ScheduleService;
+
+@RestController
+@RequestMapping
 public class ScheduleController {
+
+    // 속성
+    private final ScheduleService service;
+
+    // 생성자
+    public ScheduleController(ScheduleService service) {
+        this.service = service;
+    }
 }
